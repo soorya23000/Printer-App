046429041e34cb0539014a0b6103989ac0c9b5c8
soorya23000 <145577711+soorya23000@users.noreply.github.com>
2023-10-27 11:06:26 +0300

    initial commit

---
+++ My second project/src/app/MySecondApp.java
@@ -0,0 +1,48 @@
+package app;
+
+public class MySecondApp {
+
+	public static void main(String[] args) {
+		// trying to create my second java program
+		System.out.println(" Printer App ");
+		
+		String creater;
+		creater = " Soorya" ;
+		System.out.println(" This app is made by" + creater);
+		
+		int Number1 = 10;
+		int Number2 = 20;
+		System.out.println(Number1);
+		System.out.println(Number2);
+		
+		int number1= 20;
+		int number2= 10;
+		System.out.println(" The value of number1 is " + number1);
+		System.out.println(" The value of number2 is " + number2);
+		
+		//Multiplication
+		int multiplication;
+		multiplication = number1 * number2;
+		System.out.println (multiplication);
+		
+		//Addition
+		int addition;
+		addition = number1 + number2;
+		System.out.println (addition);
+		
+		//Substraction
+		int substraction;
+		substraction = number1-number2;
+		System.out.println (substraction);
+		
+		//Division
+		int division;
+		division = number1 / number2;
+		System.out.println (division);
+		
+				
+		
+
+	}
+
+}
